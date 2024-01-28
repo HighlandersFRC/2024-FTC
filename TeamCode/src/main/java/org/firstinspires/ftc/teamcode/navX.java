9db8f1f68236bfaf38b430303b2b12016f010444
ethanlululu <90187@psdschools.org>
2024-01-27 20:50:47 -0700

    asdfghjkl;

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navX.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+import com.kauailabs.navx.ftc.AHRS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class navX extends LinearOpMode {
+    public AHRS navX;
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+}}
