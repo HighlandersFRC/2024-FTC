dd2ab6bd50b89534ea1befe31b25752f53f6de04
Ethan Lu <90187@passchools.org>
2024-07-08 18:18:34 -0600

    fixed error in drive subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PathingTool/test.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.PathingTool;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class test extends LinearOpMode{
+    @Override
+    public void run
+}
