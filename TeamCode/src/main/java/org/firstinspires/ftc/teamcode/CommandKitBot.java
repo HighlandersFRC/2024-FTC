b3506ccaf66558633b3ff936c2d076a152308875
Ashvath Gupta <ashvathgupta9@gmail.com>
2024-12-08 18:58:22 -0800

    Fully completed manual Commands now working on additional code for the color sensor and the remaining teleOP code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandKitBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandKitBot.java
@@ -3,19 +3,30 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 import org.firstinspires.ftc.teamcode.Commands.ArmCommandUp;
-import org.firstinspires.ftc.teamcode.Commands.ArmCommmandDown;
 import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
+import org.firstinspires.ftc.teamcode.Commands.Intake;
+import org.firstinspires.ftc.teamcode.Commands.Outtake;
+import org.firstinspires.ftc.teamcode.Commands.WristCommands;
 import org.firstinspires.ftc.teamcode.Subsystems.ArmSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.Wrist;
 import org.json.JSONException;
 
 @TeleOp
 public class CommandKitBot extends LinearOpMode {
+
+    private boolean StopTheIntake;
+
     @Override
     public void runOpMode() throws InterruptedException {
 double toto = 7;
         ArmSubsystem.initialize(hardwareMap);
+        Wrist.initialize(hardwareMap);
+        IntakeSubsystem.initialize(hardwareMap);
+        Drive.initialize(hardwareMap);
         CommandScheduler scheduler = new CommandScheduler();
         waitForStart();
         while (opModeIsActive()) {
@@ -25,25 +36,34 @@ double toto = 7;
                 scheduler.schedule(new ArmCommandUp(0));
             }
 
-                  //                  if(gamepad1.right_trigger!=0) {
-//                    StopTheIntake=false;
-//                    scheduler.schedule(new Intake());
-//                } else if(gamepad1.left_trigger!=0) {
-//                    StopTheIntake=false;
-//                    scheduler.schedule(new Outtake());
-//                }
-//                  else {
-//                      StopTheIntake=true;
-//                  }
+
+            if(gamepad1.right_trigger!=0) {
+                    StopTheIntake=false;
+                    scheduler.schedule(new Intake());
+                } else if(gamepad1.left_trigger!=0) {
+                    StopTheIntake=false;
+                    scheduler.schedule(new Outtake());
+                }
+                  else {
+                      StopTheIntake=true;
+                  }
+
+                  if(gamepad1.dpad_right) {
+                      scheduler.schedule(new WristCommands(0.8));
+                  } else if(gamepad1.dpad_left) {
+                      scheduler.schedule(new WristCommands(0.2));
+                  }
+
+            Drive.FeildCentric(gamepad1);
 
             try {
                 scheduler.run();
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
-            //boolean skibidi =  Math.abs(ArmSubsystem.getCurrentPositionWithLimitSwitch() + toto) >= Math.abs(-1936);
-            //System.out.println("Current Pos" + skibidi);
-            telemetry.update();
+
+
+
 
         }
     }
